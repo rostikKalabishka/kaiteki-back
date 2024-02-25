a61c18b601ef783f95a9bd45aab5852a8fec524f
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2024-02-25 19:32:07 +0200

    add validation exception factory, fix eslint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { validationExceptionFactory } from './shared/exceptions/validation.exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      exceptionFactory: validationExceptionFactory,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
