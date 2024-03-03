c85a7e4af595052597d574b0aeb576ed92eb265b
RostikKalabishka <rostik31052002@gmail.com>
2024-03-03 17:20:36 +0200

    add user pagination

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,9 +13,9 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { AdminGuard } from 'src/auth/guards/admin.guard';
 
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
-
+import { Query as ExpressQuery } from 'express-serve-static-core';
 @UseGuards(JwtAuthGuard)
-@UseGuards(AdminGuard)
+// @UseGuards(AdminGuard)
 @Controller('user')
 export class UsersController {
   constructor(private userService: UsersService) {}
@@ -25,9 +25,13 @@ export class UsersController {
     return this.userService.findById(id);
   }
 
-  @Get()
-  findAllUsers(@Query('email') email: string) {
+  @Get('/email')
+  findAllUsersWithEmail(@Query('email') email: string) {
     return this.userService.find(email);
+  }
+  @Get('')
+  findAllUsers(@Query() query: ExpressQuery) {
+    return this.userService.findAll(query);
   }
 
   @Patch('/:id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schemas';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dtos/create-user.dto';
-
+import { Query } from 'express-serve-static-core';
 @Injectable()
 export class UsersService {
   constructor(
@@ -24,6 +24,12 @@ export class UsersService {
     return this.userModel.findOne({ email: email });
   }
 
+  async findAll(query: Query) {
+    const resPerPage = Number(query.size) || 5;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    return this.userModel.find().limit(resPerPage).skip(skip);
+  }
   async update(id: string, attrs: Partial<User>) {
     const user = await this.userModel.findById(id);
     if (!user) {
